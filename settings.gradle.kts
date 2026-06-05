f436ea6f2e31a39faf1b31a60a62adf4400c5b84
Tamim Hossain <132823494+codewithtamim@users.noreply.github.com>
2026-06-05 14:45:10 +0600

    fix(jitpack): fix YAML parsing, rename android/app to android/library

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "is-it-slop"
-include(":app")
-project(":app").projectDir = file("android/app")
+include(":library")
+project(":library").projectDir = file("android/library")
